dabadc5c33bde0672288accd6cdeb7d632554f1d
Thiago Yuri <thiago.souza@ccc.ufcg.edu.br>
2022-10-24 09:32:29 -0300

    fix: hacktoberfest year to 2022 instead of 2021

--- java/hello-thiagoyeds.java
+++ java/hello-thiagoyeds.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Hacktoberfest 2021! My name is thiagoyeds!"); 
+        System.out.println("Hello Hacktoberfest 2022! My name is thiagoyeds!"); 
     }
 }
